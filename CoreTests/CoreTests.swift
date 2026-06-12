2215f7dcf41754f0e3fb855a2f5ba22d70cb1927
Aaron Evanjulio <aaronevanjulio18@gmail.com>
2026-06-12 09:57:59 +0700

    feat: initial commit

---
+++ CoreTests/CoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreTests
+//  CoreTests
+//
+//  Created by aaronevanjulio on 12/06/26.
+//
+
+import Testing
+@testable import Core
+
+struct CoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
